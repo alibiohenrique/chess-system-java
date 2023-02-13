0f128b9c1bb07e96f80603ece2444ee915762c83
Henrique Alibio <alibio.henrique@gmail.com>
2023-02-13 12:54:52 -0300

    Created class Ui, and modified Board,Piece,Position and program

--- src/boardGame/Position.java
+++ src/boardGame/Position.java
@@ -1,40 +1,33 @@
 package boardGame;
 
 public class Position {
-	private Integer row;
-	private Integer column;
 
-	public Position() {
+	private int row;
+	private int column;
 
-	}
-
-	public Position(Integer row, Integer column) {
-
+	public Position(int row, int column) {
 		this.row = row;
 		this.column = column;
 	}
 
-	public Integer getRow() {
+	public int getRow() {
 		return row;
 	}
 
-	public void setRow(Integer row) {
+	public void setRow(int row) {
 		this.row = row;
 	}
 
-	public Integer getColumn() {
+	public int getColumn() {
 		return column;
 	}
 
-	public void setColumn(Integer column) {
+	public void setColumn(int column) {
 		this.column = column;
 	}
 
 	@Override
 	public String toString() {
-		return  row + ", " + column;
+		return row + ", " + column;
 	}
-	
-	
-
 }
